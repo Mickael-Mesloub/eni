cb7400113f7f13f4cf9d958587a98d3daf94dcde
Mickael-Mesloub <mickael.mesloub@gmail.com>
2025-10-29 10:54:33 +0100

    refactor(TpEntreprise Company.java): in showCompanyInfo(), replace companyInfo type String by StringBuilder + append string in loop

--- java/tp/TpEntreprise/src/TpEntrepriseApp.java
+++ java/tp/TpEntreprise/src/TpEntrepriseApp.java
@@ -4,13 +4,13 @@ public class TpEntrepriseApp {
         // Instances des salariés
 
         // Oranges
-        Employee orangesEmployee1 = new Employee( "Grieeman", "Natacha", 32);
+        Employee orangesEmployee1 = new Employee("Grieeman", "Natacha", 32);
         Employee orangesEmployee2 = new Employee("Dupont", "Jean-Luc", 37);
 
         Employee[] orangesEmployees = new Employee[]{orangesEmployee1, orangesEmployee2};
 
         // GoGoDev
-        Employee gogodevEmployee = new Employee("Brechet","Stephane", 29);
+        Employee gogodevEmployee = new Employee("Brechet", "Stephane", 29);
 
         Employee[] gogodevEmployees = new Employee[]{gogodevEmployee};
 
@@ -19,7 +19,7 @@ public class TpEntrepriseApp {
         Employee nextdigitalEmployee2 = new Employee("Durand", "Esteban", 31);
         Employee nextdigitalEmployee3 = new Employee("Laporte", "Mélanie", 34);
 
-        Employee[] nextdigitalEmployees = new Employee[]{nextdigitalEmployee1, nextdigitalEmployee2,nextdigitalEmployee3};
+        Employee[] nextdigitalEmployees = new Employee[]{nextdigitalEmployee1, nextdigitalEmployee2, nextdigitalEmployee3};
 
         // Instances des adresses
         Address orangesAddress = new Address("44300", "Nantes");
@@ -32,13 +32,13 @@ public class TpEntrepriseApp {
         Company nextdigital = new Company("NextDigital", nextdigitalAddress, nextdigitalEmployees);
 
         Company[] companies = new Company[]{oranges, gogodev, nextdigital};
-        
+
         getCompaniesInfo(companies);
     }
 
     static void getCompaniesInfo(Company[] companies) {
         // Boucler sur les entreprises pour afficher leurs infos
-        for(int i = 0; i < companies.length; i++) {
+        for (int i = 0; i < companies.length; i++) {
             companies[i].showCompanyInfo();
         }
     }
