cc0712f9faa460a58338c3bfee70b8b6dfc4694f
Mickael-Mesloub <mickael.mesloub@gmail.com>
2025-10-30 15:08:23 +0100

    refactor(TpInventory): replace for loops with stream() and add quantity updates

--- java/tp/TpInventory/src/Inventory.java
+++ java/tp/TpInventory/src/Inventory.java
@@ -10,27 +10,55 @@ public class Inventory {
     }
 
     // Méthode pour ajouter un item à la liste
-    public void addItem(Item newItem){
-        items.add(newItem);
-        System.out.println(String.format("%s x%d ajouté à l'inventaire.", newItem.name, newItem.quantity));
+    public void addItem(Item item){
+        // Si item existe, on le cherche dans la liste
+        // on cherche le premier item qui a le même nom
+        // Si on trouve pas, null (orElse(null)
+       Item foundItem = items.stream().filter((i ) -> i.name.equals(item.name)).findFirst().orElse(null);
+
+       // Si j'ai trouvé cet item, j'augmente sa quantité
+       if(foundItem != null) {
+           foundItem.quantity += item.quantity;
+           // Message d'ajout de quantité d'un objet existant
+           System.out.println(String.format("➕ %s x%d ajouté à l'inventaire.", item.name, item.quantity));
+           return;
+       }
+
+       // S'il n'existe pas, je l'ajoute à l'inventaire
+       items.add(item);
+
+       // Message d'ajout de nouvel item
+        System.out.println(String.format("\uD83C\uDF1F Nouvel item %s x%d ajouté à l'inventaire.", item.name, item.quantity));
     };
 
     // Méthode pour afficher les items de la liste
     public void displayItems(){
-        StringBuilder message = new StringBuilder(String.format("Contenu de l\'inventaire :\n"));
+        StringBuilder message = new StringBuilder(String.format("\nContenu de l\'inventaire :\n"));
         for(Item item : items) {
-            message.append(String.format("%s x%d\n", item.name, item.quantity));
+            message.append(String.format("• %s x%d\n", item.name, item.quantity));
         }
 
         System.out.println(message);
     }
 
-    // TODO: test method
     // Méthode pour supprimer un item de la liste
-    public void removeItem(String nameOfItemToRemove){
-        for(Item item : items) {
-            if(item.name.equals(nameOfItemToRemove)) {
-                items.remove(item);
+    public void removeItem(Item item){
+        // Boucler sur la liste d'items pour chercher l'item
+        Item foundItem = items.stream().filter((i ) -> i.name.equals(item.name)).findFirst().orElse(null);
+
+        // Si l'item existe dans l'inventaire
+        if(foundItem != null) {
+            // Si quantité indiquée >= quantité dans l'inventaire, supprimer totalement l'item
+            if(item.quantity >= foundItem.quantity) {
+                items.remove(foundItem);
+                // Message de suppression totale d'un objet
+                System.out.println(String.format("❌ %s supprimé de l'inventaire.", foundItem.name));
+
+            } else {
+                // Sinon, juste diminuer la quantité dans l'inventaire
+                foundItem.quantity -= item.quantity;
+                // Message de suppression d'une quantité d'un objet existant
+                System.out.println(String.format("➖ %s x%d supprimé de l'inventaire.", item.name, item.quantity));
             }
         }
     }
--- java/tp/TpInventory/src/TpInventoryApp.java
+++ java/tp/TpInventory/src/TpInventoryApp.java
@@ -8,24 +8,35 @@ public class TpInventoryApp {
 
         // Instancier les items
         Item sword = new Item("Épée", 1);
+        Item sword2 = new Item("Épée", 4);
         Item shield = new Item("Bouclier", 1);
         Item healthPotion = new Item("Potion de vie", 5);
         Item brokenBow = new Item("Arc brisé", 1);
         Item brokenArrow = new Item("Flêche cassée", 8);
+        Item sword3 = new Item("Épée", 1);
 
         // Liste d'items à ajouter
-        ArrayList<Item> itemsToAdd = new ArrayList<Item>(List.of(sword, shield, healthPotion, brokenBow,
+        ArrayList<Item> itemsToAdd = new ArrayList<Item>(List.of(sword, sword2, shield, healthPotion, brokenBow,
                 brokenArrow));
 
         // Liste d'items à supprimer
-        ArrayList<Item> itemsToRemove = new ArrayList<Item>(List.of(brokenBow, brokenArrow));
+        ArrayList<Item> itemsToRemove = new ArrayList<Item>(List.of(brokenBow, brokenArrow, sword3));
 
         for (Item item : itemsToAdd) {
             inventory.addItem(item);
         }
 
+        // Afficher le contenu de l'inventaire
         inventory.displayItems();
 
-        //TODO: test removeItem()
+        // Supprimer des items
+        for (Item item : itemsToRemove) {
+            inventory.removeItem(item);
+        }
+
+        // Réafficher l'inventaire mis à jour
+        inventory.displayItems();
+
     }
+
 }
