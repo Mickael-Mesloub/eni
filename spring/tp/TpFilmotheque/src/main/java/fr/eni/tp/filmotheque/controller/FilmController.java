ce2e785036d8ec215e924a1c12461cb47c8470d2
Mickael-Mesloub <mickael.mesloub@gmail.com>
2025-11-27 14:57:36 +0100

    feat(TpFilmotheque): add Spring web & Thymleaf. Add header fragment + film table + film detail page with dynamic route

--- spring/tp/TpFilmotheque/src/main/java/fr/eni/tp/filmotheque/controller/FilmController.java
+++ spring/tp/TpFilmotheque/src/main/java/fr/eni/tp/filmotheque/controller/FilmController.java
@@ -1,9 +1,18 @@
 package fr.eni.tp.filmotheque.controller;
 
 import fr.eni.tp.filmotheque.bll.FilmService;
-import org.springframework.stereotype.Component;
+import fr.eni.tp.filmotheque.bo.Film;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+import java.util.List;
+
+@Controller
+@RequestMapping("/films")
 public class FilmController {
     FilmService filmService;
 
@@ -11,11 +20,19 @@ public class FilmController {
         this.filmService = filmService;
     }
 
-    public void afficherFilms() {
-        this.filmService.consulterFilms().forEach(System.out::println);
+    @GetMapping
+    public String getFilms(Model model) {
+        List<Film> films = filmService.consulterFilms();
+        model.addAttribute("films", films);
+        return "view-films";
     }
 
-    public void afficherUnFilm(int id) {
-        System.out.println(this.filmService.consulterFilmParId(id));
+    @GetMapping("/detail")
+    public String getFilmDetails(@RequestParam long id, Model model) {
+        Film film = filmService.consulterFilmParId(id);
+        model.addAttribute("film", film);
+
+        return "view-film-detail";
     }
+
 }
