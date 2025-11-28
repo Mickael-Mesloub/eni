5530742755c0041f4db5f2cb4f1b21a9cba3c39f
Mickael-Mesloub <mickael.mesloub@gmail.com>
2025-11-28 16:28:28 +0100

    WIP(TpFilmotheque): move header fragment in /fragments, add link to /films/creer + methods + view

--- spring/tp/TpFilmotheque/src/main/java/fr/eni/tp/filmotheque/controller/FilmController.java
+++ spring/tp/TpFilmotheque/src/main/java/fr/eni/tp/filmotheque/controller/FilmController.java
@@ -2,21 +2,25 @@ package fr.eni.tp.filmotheque.controller;
 
 import fr.eni.tp.filmotheque.bll.FilmService;
 import fr.eni.tp.filmotheque.bo.Film;
+import fr.eni.tp.filmotheque.bo.Genre;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@SessionAttributes({"listeGenresEnSession"})
 @Controller
 @RequestMapping("/films")
 public class FilmController {
     FilmService filmService;
+    List<Genre> listeGenres;
 
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
+        this.listeGenres = new ArrayList<>();
+        listeGenres.addAll(this.filmService.consulterGenres());
     }
 
     @GetMapping
@@ -34,4 +38,22 @@ public class FilmController {
         return "view-film-detail";
     }
 
+    @GetMapping("/creer")
+    public String creer(Model model) {
+        model.addAttribute("listeGenres", filmService.consulterGenres());
+        return "view-creer-film";
+    }
+
+    @PostMapping("/creer")
+    public String creerFilm(@ModelAttribute("film") Film film) {
+        filmService.creerFilm(film);
+
+        return "redirect:/films";
+    }
+
+    @ModelAttribute("listeGenresEnSession")
+    public List<Genre> chargerGenresEnSession(){
+        System.out.println("Appel à la méthode chargerGenresEnSession");
+        return this.listeGenres;
+    }
 }
