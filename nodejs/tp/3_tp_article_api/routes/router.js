7b8961976a41b53bb660dd55c2406403d94ba0d9
Mickael-Mesloub <mickael.mesloub@gmail.com>
2026-01-27 16:29:05 +0100

    nodejs tp3: add validation if title already exists + add status code

--- nodejs/tp/3_tp_article_api/routes/router.js
+++ nodejs/tp/3_tp_article_api/routes/router.js
@@ -1,106 +1,126 @@
-const express = require('express');
-const { Article } = require('../model/Article');
-
-// Simulation de données en mémoire
-// let articles = [
-//     { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
-//     { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
-//     { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
-// ];
+const express = require("express");
+const { Article } = require("../model/Article");
 
 const router = express.Router();
 
-router.get('/', (request, response) => {
-    return response.json({
-        message: 'Bienvenue sur le TP Article API !',
-    });
+// get root
+router.get("/", (request, response) => {
+  return response.json({
+    message: "Bienvenue sur le TP Article API !",
+  });
 });
 
-router.get('/articles', async (request, response) => {
-    const articles = await Article.find();
+// get all articles
+router.get("/articles", async (request, response) => {
+  const articles = await Article.find();
 
-    return response.json({
-        message: 'Voici la liste des articles !',
-        articles
-    });
+  return response.json({
+    status: 200,
+    message: "Voici la liste des articles !",
+    data: articles,
+  });
 });
 
-router.get('/articles/:id', async (request, response) => {
-    const id = Number(request.params.id);
+// get article by id
+router.get("/articles/:id", async (request, response) => {
+  const id = Number(request.params.id);
 
-    if(Number.isNaN(id)) {
-        return response.json({
-            error: "Format d'id incorrect. Il ne doit comporter que des chiffres."
-        })
-    }
-
-    const article =  await Article.findOne({id});    
-
-    if (!article) {
-        return response.json({
-            error: `Article avec l'id ${id} introuvable`
-        });
-    }
-
+  if (Number.isNaN(id)) {
     return response.json({
-        message: `Voici l'article ${id} !`,
-        article
+      status: 400,
+      message:
+        "Erreur : Format d'id incorrect. Il ne doit comporter que des chiffres.",
     });
+  }
+
+  const article = await Article.findOne({ id });
+
+  if (!article) {
+    return response.json({
+      status: 404,
+      message: `Erreur : Article avec l'id ${id} introuvable`,
+    });
+  }
+
+  return response.json({
+    status: 200,
+    message: `Voici l'article ${id} !`,
+    data: article,
+  });
 });
 
-router.post('/save-article', async (request, response) => {
-    const { title, content, author } = request.body;
-    const articles = await Article.find();
-    let id;
+// create article
+router.post("/save-article", async (request, response) => {
+  const { title, content, author } = request.body;
+  let id;
 
-    if(articles.length === 0) {
-        id = 1;
-    } else {
-        id = Math.max(...articles.map(a => a.id)) + 1;
-    }
-    
-    const newArticle = new Article({
-        id,
-        title,
-        content,
-        author
-    });
+  // TODO: data validation
 
-    await newArticle.save();
+  const articles = await Article.find();
 
+// Validation - article already exists with title
+  const articleFoundWithTitle = await Article.findOne({ title });
+
+  if (articleFoundWithTitle) {
     return response.json({
-        message: `Le nouvel article avec l'id ${newArticle.id} a été ajouté !`,
-        newArticle
+      status: 701,
+      message: `Erreur : Un article avec le titre "${title}" existe déjà`,
     });
-    
+  }
 
+  // ------------------------------------------ \\
+
+  if (articles.length === 0) {
+    id = 1;
+  } else {
+    id = Math.max(...articles.map((a) => a.id)) + 1;
+  }
+
+  const newArticle = new Article({
+    id,
+    title,
+    content,
+    author,
+  });
+
+  await newArticle.save();
+
+  return response.json({
+    status: 201,
+    message: `Le nouvel article avec l'id ${newArticle.id} a été ajouté !`,
+    data: newArticle,
+  });
 });
 
-router.delete('/article/:id', async (request, response) => {
-    const id = Number(request.params.id);
+// delete article by id
+router.delete("/article/:id", async (request, response) => {
+  const id = Number(request.params.id);
 
-    if(Number.isNaN(id)) {
-        return response.json({
-            error: "Format d'id incorrect. Il ne doit comporter que des chiffres."
-        })
-    }
-
-    const articleFound = await Article.findOne({id});
-
-    if(!articleFound) {
-        return response.json({
-            error: `Article avec l'id ${id} introuvable`
-        })
-    }
-
-    await articleFound.deleteOne();
-    
+  if (Number.isNaN(id)) {
     return response.json({
-        message: `Article avec l'id ${id} supprimé !`,
-        articleFound
+      status: 400,
+      message:
+        "Erreur : Format d'id incorrect. Il ne doit comporter que des chiffres.",
     });
+  }
+
+  const articleFound = await Article.findOne({ id });
+
+  if (!articleFound) {
+    return response.json({
+      status: 404,
+      message: `Erreur : Article avec l'id ${id} introuvable`,
+    });
+  }
+
+  await articleFound.deleteOne();
+
+  return response.json({
+    status: 200,
+    message: `Article avec l'id ${id} supprimé !`,
+  });
 });
 
 module.exports = {
-    router
-}
+  router,
+};
