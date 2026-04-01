2529f7fbcb9896628f524c3ed7605028b9aba264
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-04-01 15:38:21 +0200

    TP06 Cave à vin : add validation

--- springboot-avance/tp/cave-a-vin/src/main/java/fr/eni/cave/bo/vin/Bouteille.java
+++ springboot-avance/tp/cave-a-vin/src/main/java/fr/eni/cave/bo/vin/Bouteille.java
@@ -2,6 +2,10 @@ package fr.eni.cave.bo.vin;
 
 import jakarta.persistence.*;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @AllArgsConstructor
@@ -16,25 +20,31 @@ public class Bouteille {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank
+    @Size(max = 250)
     @Column(name = "NAME", length = 250, nullable = false, unique = true)
     private String nom;
 
     @Column(name = "SPARKLING")
     private Boolean petillant;
 
+    @Size(min = 4, max = 4)
     @Column(name = "VINTAGE")
     private String millesime;
 
+    @Min(1)
     @Column(name = "QUANTITY")
     private Integer quantite;
-
+    @Min(1)
     @Column(name = "PRICE", precision = 2)
     private Float prix;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "COLOR_ID")
     private Couleur couleur;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "REGION_ID")
     private Region region;
