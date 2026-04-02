75ec90912d332e1347ea8a620399c037825d458d
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-04-02 10:15:47 +0200

    Spring boot TP06 cave-a-vin-mongodb : add Rest repositories starter

--- springboot-avance/tp/cave-a-vin-mongodb/src/main/java/fr/eni/tp/dal/AvisRepository.java
+++ springboot-avance/tp/cave-a-vin-mongodb/src/main/java/fr/eni/tp/dal/AvisRepository.java
@@ -3,10 +3,12 @@ package fr.eni.tp.dal;
 import fr.eni.tp.bo.Avis;
 import fr.eni.tp.bo.vin.Bouteille;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "avis", path = "avis")
 public interface AvisRepository extends MongoRepository<Avis, String > {
     List<Avis> findByNoteLessThan(int note);
     List<Avis> findByNoteGreaterThanEqual(int note);
