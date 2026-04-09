5647c17f4f3a1679d2c9a40ad63782077f69fe6a
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-04-09 16:11:39 +0200

    Angular M04 - TP-Auth: Init register

--- angular/tp/M04/TP-Auth/src/app/pages/auth/register/register.ts
+++ angular/tp/M04/TP-Auth/src/app/pages/auth/register/register.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../../types/user';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register implements OnInit {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  users: User[] = [];
+
+  ngOnInit() {
+    const usersFromLocalStorage = localStorage.getItem('users');
+    this.users = usersFromLocalStorage ? JSON.parse(usersFromLocalStorage) : [];
+    console.log([...this.users]);
+  }
+
+  submit(): void {
+    const newUser: User = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    };
+
+    const userExists: boolean = this.users.some((user) => user.email === this.email || user.username === this.username);
+    
+    if(userExists) return;
+
+    this.users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(this.users));
+    console.log([...this.users]);
+  }
+}
