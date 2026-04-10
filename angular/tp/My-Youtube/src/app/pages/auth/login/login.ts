0283adda05678e184e3869150d7a716a1e057118
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-04-10 16:19:11 +0200

    Angular My-Youtube: setup login register

--- angular/tp/My-Youtube/src/app/pages/auth/login/login.ts
+++ angular/tp/My-Youtube/src/app/pages/auth/login/login.ts
@@ -1,22 +1,42 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
+import { LoginBody } from '../../../types/auth/login-body';
+import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private readonly authService: AuthService = inject(AuthService);
   private readonly router: Router = inject(Router);
+  
   login: string = '';
   password: string = '';
 
   invalidCredentialsMessage: string | null = null;
 
   submit() {
-    console.log('Login submitted');
+    const body: LoginBody = {
+      login: this.login,
+      password: this.password 
+    };
+
+    console.log("BODY : ", body);
+    
+
+   const isLoggedIn = this.authService.login(body);
+
+   if(!isLoggedIn) {
+      this.invalidCredentialsMessage = "Identifiants incorrects";
+
+      return;
+   }
+
+   this.router.navigate(['/']);
   }
 
   navigateToRegister(): void {
---
+++ angular/tp/My-Youtube/src/app/services/auth.ts
@@ -0,0 +1,76 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { User, UserInStorage } from '../types/user';
+import { RegisterBody } from '../types/auth/register-body';
+import { LoginBody } from '../types/auth/login-body';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  users: User[] = [];
+  private readonly currentUserSignal = signal<User | null>(null);
+  public isAuthenticated = computed(() => this.currentUserSignal() !== null);
+  public currentUser = computed(() => this.currentUserSignal());
+
+  constructor() {
+    const storedUser = sessionStorage.getItem('currentUser');
+
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      this.currentUserSignal.set(user);
+    }
+    const usersFromLocalStorage = localStorage.getItem('users');
+    this.users = usersFromLocalStorage ? JSON.parse(usersFromLocalStorage) : [];
+    console.log([...this.users]);
+  }
+
+  login(body: LoginBody): boolean {
+    if (!this.validateCredentials(body.login, body.password)) {
+      console.log(body.login, ' / ', body.password);
+      return false;
+    }
+
+    const user: UserInStorage = {
+      username: body.login.trim(),
+      token: 'jwt-token',
+    };
+
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    console.log('Connexion réussie !');
+
+    return true;
+  }
+
+  register(body: RegisterBody): boolean {
+    // Create new user
+    const newUser: User = {
+      username: body.username.trim(),
+      email: body.email.trim(),
+      password: body.password.trim(),
+    };
+
+    // Check if user already exists in array in localStorage
+    const userExists: boolean = this.users.some(
+      (user) =>
+        user.email.trim() === body.email.trim() || user.username.trim() === body.username.trim(),
+    );
+
+    // If user exists, return nothing to stop execution
+    if (userExists) return false;
+
+    // Add user in array of users
+    this.users.push(newUser);
+
+    // Add array in localStorage
+    localStorage.setItem('users', JSON.stringify(this.users));
+    console.log('Inscription réussie !');
+
+    return true;
+  }
+
+  validateCredentials(login: string, password: string): boolean {
+    console.log(this.users);
+    
+    return this.users.some((user) => user.username === login && user.password === password);
+  }
+}
