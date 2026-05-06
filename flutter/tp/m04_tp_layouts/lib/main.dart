10e53d1bb26445b403f3225e835183fa532e95d7
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-05-06 10:15:44 +0200

    Flutter TP: stateful MyHomePage with dynamic content rendering with bottomNavbar

--- flutter/tp/m04_tp_layouts/lib/main.dart
+++ flutter/tp/m04_tp_layouts/lib/main.dart
@@ -24,24 +24,90 @@ class MyApp extends StatelessWidget {
           titleLarge: TextStyle(
             fontSize: 24,
             color: colorScheme.primary,
-            fontWeight: FontWeight(700)
-          )
+            fontWeight: FontWeight(700),
+          ),
         ),
         bottomNavigationBarTheme: BottomNavigationBarThemeData(
           backgroundColor: Colors.black,
           unselectedItemColor: Colors.grey,
           selectedItemColor: Colors.blue,
-        )
+        ),
       ),
-      home: MyHomePage(title: 'TP Layouts'),
+      home: MyHomePage(title: 'TP Mes projets'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({super.key, required this.title});
 
   final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPageIndex = 0;
+  Widget _homePage = HomePage();
+  Widget _contributionPage = ContributionPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ***************
+      // *** APP BAR ***
+      // ***************
+      appBar: AppBar(
+        leading: Icon(Icons.rocket_launch, color: Colors.white),
+        elevation: 8,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+        foregroundColor: Colors.white,
+        title: Text(widget.title),
+      ),
+
+      // ***************
+      // *** NAVIGATION BAR ***
+      // ***************
+      bottomNavigationBar: NavigationBar(
+        indicatorColor: Colors.blueAccent,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.folder_open),
+            label: 'Projets',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Contribuer',
+          ),
+        ],
+      ),
+      // ***************
+      // *** BODY ***
+      // ***************
+      body: this.getBody(),
+    );
+  }
+
+  Widget getBody() {
+    List<Widget> pages = [_homePage, _contributionPage];
+
+    return pages[currentPageIndex];
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
   final List<Project> projects = [
     Project("Projet 1", "Spring boot"),
@@ -50,66 +116,51 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.rocket_launch, color: Colors.white),
-        elevation: 8,
-        shadowColor: Colors.black,
-        backgroundColor: Colors.indigo,
-        centerTitle: true,
-        foregroundColor: Colors.white,
-        title: Text(title),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.folder_open),
-            label: 'Projets',
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Text(
+              'Liste de projets',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: 'Contribuer',
+          Expanded(
+            child: ListView.builder(
+              itemCount: projects.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Colors.black,
+                  elevation: 4,
+                  child: ListTile(
+                    leading: Icon(Icons.folder),
+                    title: Text(
+                      projects[index].title,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      projects[index].description,
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    trailing: Icon(Icons.chevron_right),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 16),
-              child: Text(
-                'Liste de projets',
-                  style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: projects.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    color: Colors.black,
-                    elevation: 4,
-                    child: ListTile(
-                      leading: Icon(Icons.folder),
-                      title: Text(
-                        projects[index].title,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      subtitle: Text(
-                        projects[index].description,
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      trailing: Icon(Icons.chevron_right),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
     );
+  }
+}
+
+class ContributionPage extends StatelessWidget {
+  ContributionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Page de contribution');
   }
 }
